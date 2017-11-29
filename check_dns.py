f4fb6dceaec2d3b8ff619f58fa04a86e1549de55
Pieter Lexis <pieter@plexis.eu>
2017-11-29 16:42:29 +0100

    Add RRSIG expiry test

--- check_dns.py
+++ check_dns.py
@@ -5,6 +5,7 @@
 
 from __future__ import print_function, absolute_import
 import argparse
+import time
 from collections import OrderedDict
 import dnsviz.commands.probe
 from dnsviz.analysis import TTLAgnosticOfflineDomainNameAnalysis, DNS_RAW_VERSION
@@ -40,13 +41,31 @@ class RRSIGContext(nagiosplugin.Context):
             return nagiosplugin.Result(nagiosplugin.Critical, 'RRSIG error', metric)
         if metric.value['warnings']:
             return nagiosplugin.Result(nagiosplugin.Warn, 'RRSIG issue', metric)
-        return nagiosplugin.Result(nagiosplugin.Ok, 'RRSIGs are good', metric)
+        return nagiosplugin.Result(nagiosplugin.Ok, 'RRSIGs validate correctly', metric)
+
+
+class RRSIGExpitationContext(nagiosplugin.Context):
+    def __init__(self, name, warn_seconds, crit_seconds, fmt_metric=None, result_cls=nagiosplugin.Result):
+        super(RRSIGExpitationContext, self).__init__(name, fmt_metric, result_cls)
+        self.warn_seconds = warn_seconds
+        self.crit_seconds = crit_seconds
+
+    def evaluate(self, metric, resource):
+        r_state = nagiosplugin.Ok
+        if self.warn_seconds >= metric.value:
+            r_state = nagiosplugin.Warn
+        if self.crit_seconds >= metric.value:
+            r_state = nagiosplugin.Critical
+        return nagiosplugin.Result(r_state, 'RRSIGs expiring in {} seconds ({} hours)'.format(
+            metric.value, metric.value/3600), metric)
 
 
 class DNS(nagiosplugin.Resource):
-    def __init__(self, domain, insecure_ok=True):
+    def __init__(self, domain, insecure_ok=True, rrsig_expiration_warn=72, rrsig_expiration_crit=24):
         self.domain = domain
         self.insecure_ok = insecure_ok
+        self.rrsig_expiration_warn = rrsig_expiration_warn
+        self.rrsig_expiration_crit = rrsig_expiration_crit
 
         self.try_ipv4 = True
         self.try_ipv6 = True
@@ -102,14 +121,25 @@ class DNS(nagiosplugin.Resource):
 
         rrsig_errors = set()
         rrsig_warnings = set()
+        rrsig_expiration = None
+        now = int(time.time())
         for _, rrsigs in analysis_obj.rrsig_status.iteritems():
-            for _, rrsets in rrsigs.iteritems():
+            for rrsig, rrsets in rrsigs.iteritems():
                 for keymeta, single_rrsig_status in rrsets.iteritems():
                     for w in single_rrsig_status.warnings:
                         rrsig_warnings.add('{}: {}'.format(keymeta, w.description))
                     for e in single_rrsig_status.errors:
                         rrsig_errors.add('{}: {}'.format(keymeta, e.description))
-        yield nagiosplugin.Metric('rrsig_status', {'errors': rrsig_errors, 'warnings': rrsig_warnings}, context='rrsig')
+                    expire_seconds = rrsig.expiration - now
+                    if rrsig_expiration is None or expire_seconds < rrsig_expiration:
+                        rrsig_expiration = expire_seconds
+        yield nagiosplugin.Metric('rrsig_status',
+                                  {'errors': rrsig_errors,
+                                   'warnings': rrsig_warnings},
+                                  context='rrsig')
+        yield nagiosplugin.Metric('rrsig_expiration',
+                                  rrsig_expiration,
+                                  context='rrsig_expiration')
 
 
 class DNSSummary(nagiosplugin.Summary):
@@ -136,6 +166,10 @@ class DNSSummary(nagiosplugin.Summary):
                     ret.append('{}: {}'.format(result.hint, to_add))
                 else:
                     ret.append(result.hint)
+
+            if result.metric.name == 'rrsig_expiration':
+                ret.append(result.hint)
+
         return '; '.join(ret)
 
     def problem(self, results):
@@ -160,20 +194,37 @@ class DNSSummary(nagiosplugin.Summary):
                     ret.append('{}: {}'.format(result.hint, to_add))
                 else:
                     ret.append(result.hint)
+            if result.metric.name == 'rrsig_expiration':
+                ret.append(result.hint)
         return '; '.join(ret)
 
 
 @nagiosplugin.guarded()
 def main():
-    argp = argparse.ArgumentParser()
-    argp.add_argument('--domain', '-d', type=str, required=True, action='store')
-    argp.add_argument('--insecure-is-ok', action='store_true', default=False)
-    argp.add_argument('--verbose', '-v', action='count', default=0)
+    argp = argparse.ArgumentParser(prog='check_dns', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argp.add_argument('--domain', '-d', type=str, required=True, action='store',
+                      help='Domain to check')
+    argp.add_argument('--insecure-is-ok', action='store_true', default=False,
+                      help='If the delegation is insecure, do not warn')
+    argp.add_argument('--expire-warn', default=72, metavar='HOURS',
+                      help='Warn if RRSIG expiry is within this many hours')
+    argp.add_argument('--expire-crit', default=48, metavar='HOURS',
+                      help='Crit if RRSIG expiry is within this many hours')
+    argp.add_argument('--verbose', '-v', action='count', default=0,
+                      help='Be more verbose')
     args = argp.parse_args()
+
+    if args.expire_crit > args.expire_warn:
+        raise nagiosplugin.CheckError('RRSIG Critical value ({}) higher than Warning value ({})'.format(
+            args.expire_crit, args.expire_warn
+        ))
     check = nagiosplugin.Check(
         DNS(args.domain, args.insecure_is_ok),
         DNSSECContext('dnssec'),
         RRSIGContext('rrsig'),
+        RRSIGExpitationContext('rrsig_expiration',
+                               warn_seconds=args.expire_warn * 60 * 60,
+                               crit_seconds=args.expire_crit * 60 * 60),
         DNSSummary())
     check.main(args.verbose)
 
